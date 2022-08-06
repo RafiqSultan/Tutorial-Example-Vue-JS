5437d8643b44d405c54ca578cfb5bd0b7603146a
RafiqSultan <Rafiqaasultan@gmail.com>
2022-08-06 08:31:40 -0700

    ADD change color wesite

---
+++ src/views/ChangColor.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <h1>Chang website color</h1>
+    <div :style="{ '--main-color': color }">
+      <ul>
+        <li data-color="red" @click="updateColor"></li>
+        <li data-color="rgb(10, 40, 80)" @click="updateColor"></li>
+        <li data-color="rgb(27, 207, 177)" @click="updateColor"></li>
+        <li data-color="rgb(129, 11, 113)" @click="updateColor"></li>
+      </ul>
+      <div class="color">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      color: "red",
+    };
+  },
+  methods: {
+    updateColor(e) {
+      this.color = e.target.getAttribute("data-color");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+ul {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  li {
+    list-style-type: none;
+    margin: 10px 5px;
+    width: 30px;
+    height: 30px;
+    background-color: #000;
+    cursor: pointer;
+  }
+  li:first-child {
+    background-color: red;
+  }
+  li:nth-child(2) {
+    background-color: rgb(10, 40, 80);
+  }
+  li:nth-child(3) {
+    background-color: rgb(27, 207, 177);
+  }
+  li:last-child {
+    background-color: rgb(129, 11, 113);
+  }
+}
+.color {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  div {
+    margin: 10px;
+    width: 150px;
+    height: 50px;
+    background-color: var(--main-color);
+  }
+}
+</style>
---
+++ src/views/ShowAndHidePassword.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Show and hide password</h1>
+    <form action="#">
+      <input :type="feildType" placeholder="input password" />
+      <button @click.prevent="switchFeild">{{ ShowAndHide }}</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      ShowAndHide: "Show",
+      feildType: "password",
+    };
+  },
+  methods: {
+    switchFeild() {
+      // this.feildType==="password" ? "text "this.ShowAndHide="Hide" :"password",this.ShowAndHide="password";
+      if (this.feildType === "password") {
+        this.ShowAndHide = "Hide";
+        this.feildType = "text";
+      } else {
+        this.ShowAndHide = "Show";
+        this.feildType = "password";
+      }
+    },
+  },
+};
+</script>
