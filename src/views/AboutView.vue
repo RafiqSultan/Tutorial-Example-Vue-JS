645ebaf04226f79210a024fa4320c72e1630a6cb
RafiqSultan <Rafiqaasultan@gmail.com>
2022-08-06 07:40:14 -0700

    Create show and hide password

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>Show and hide password</h1>
+    <form action="#">
+      <input :type="feildType" placeholder="input password" />
+      <button @click.prevent="switchFeild">{{ ShowAndHide }}</button>
+    </form>
   </div>
 </template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      ShowAndHide: "Show",
+      feildType: "password",
+    };
+  },
+  methods: {
+    switchFeild() {
+      // this.feildType==="password" ? "text "this.ShowAndHide="Hide" :"password",this.ShowAndHide="password";
+      if (this.feildType === "password") {
+        this.ShowAndHide = "Hide";
+        this.feildType = "text";
+      } else {
+        this.ShowAndHide = "Show";
+        this.feildType = "password";
+      }
+    },
+  },
+};
+</script>
